ab8eceb0183d21a55c99d15364ecbb288436826d
Rachel Thompson <thompson.1657@gmail.com>
2020-12-15 20:48:52 -0600

    base files and links

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+//https://corona.lmao.ninja/docs/#/COVID-19%3A%20Worldometers/get_v3_covid_19_countries
+
+// var queryURL = "https://disease.sh/v3/covid-19/countries"
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response1) {
+//     console.log(response1)
+// });
+
+
+
+
+
+//https://api.covidtracking.com
+
+// var queryURL = "https://api.covidtracking.com/v1/states/current.json"
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response2) {
+//     console.log(response2)
+// });
