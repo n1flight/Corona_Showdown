1b44f700c2f4d2d59e4f9274176c4cee8238332d
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-12-17 19:33:17 -0600

    css styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+$(document).foundation();
 
-
-//https://corona.lmao.ninja/docs/#/COVID-19%3A%20Worldometers/get_v3_covid_19_countries
+// https://corona.lmao.ninja/docs/#/COVID-19%3A%20Worldometers/get_v3_covid_19_countries
 
 // var queryURL = "https://disease.sh/v3/covid-19/countries"
 // $.ajax({
